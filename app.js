61005512a615a0770558d3f0c2861a3b046b754a
Mamata24 <nmamta240@gmail.com>
2020-11-18 16:00:08 +0530

    Infinite Scrolling

---
+++ app.js
@@ -0,0 +1,31 @@
+async function loadImages() {
+    const imageDiv = document.createElement('div');
+    imageDiv.className = 'one-fourth';
+
+    const imgElement = document.createElement('img');
+    imgElement.src = `https://placekitten.com/${generateSize(150)}/${generateSize(200)}`;
+
+    imageDiv.append(imgElement);
+    document.querySelector('.container').append(imageDiv);
+}
+function generateSize(min) {
+    var randomNumber = Math.ceil(Math.random() * 200);
+    return randomNumber + min;
+}
+
+
+
+function getRandomImages(number) {
+    for (var i = 0; i < number; i++) {
+        loadImages();
+    }
+}
+
+
+getRandomImages(25);
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY + window.innerHeight + 100 >= document.documentElement.scrollHeight) {
+        fetchRandomImages(25);
+    }
+})
